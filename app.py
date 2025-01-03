8b4e59f7b090ebe2f2f1cbae846c8dcbe41871a2
Eunieversity <eyun@uni-osnabrueck.de>
2025-01-03 14:20:21 +0100

    import fontawsome

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask, request, render_template
+from flask_fontawesome import FontAwesome
 from whoosh.index import open_dir
 from whoosh.qparser import QueryParser
 
-app = Flask(__name__)
 
+app = Flask(__name__)
+fa = FontAwesome(app)
 @app.route("/")
 def home():
-    return """ <form action="/search" method="get">
-            <input type="text" name="q" placeholder="Search">
-            <button type="submit">Search</button>
-        </form>
-    """
+    return render_template('home_page.html')
+
+@app.route('/crawl')
+def search():
+    return render_template('test.html')
 
 
+app.run(host="0.0.0.0", port=80)
