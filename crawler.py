0e23ff28df7e02ec9891b0a0e8e9ff1afbe0cb66
Eunieversity <eyun@uni-osnabrueck.de>
2025-01-06 16:23:26 +0100

    delete unnecessary file

--- crawler.py
+++ crawler.py
@@ -4,26 +4,7 @@ from whoosh.index import create_in
 from whoosh.fields import Schema, TEXT, ID
 import os
 
-
-def crawl_old(start_url, base_url):
-    visited = set()
-    index = {}
-
-    def is_same_server(url):
-        return url.startswith(base_url)
-
-    def add_to_index(url, text):
-        words = text.split()
-        for word in words:
-            word = word.lower()
-            if word not in index:
-                index[word] = []
-            if url not in index[word]:
-                index[word].append(url)
-
-
 # to look up words in the index
-
 def add_to_index(url, text, index):
     words = text.split()
     for word in words:
@@ -65,26 +46,6 @@ def crawl():
                 add_to_index(url, soup.get_text(), index)
 
         return index
-
-
-# def crawl_page(url):
-#     try:
-#         response = requests.get(url)
-#         if "text/html" in response.headers["Content-Type"]:
-#             soup = BeautifulSoup(response.text, "html.parser")
-#             add_to_index(url, soup.get_text())
-#             for link in soup.find_all("a", href=True):
-#                 full_url = link["href"]
-#                 if is_same_server(full_url) and full_url not in visited:
-#                     visited.add(full_url)
-#                     crawl_page(full_url)
-#     except Exception as e:
-#         print(f"Error Processing {url}: {e}")
-#
-# visited.add(start_url)
-# crawl_page(start_url)
-# return index
-
 
 def search(index, words):
     results = []
